28193f1c8e405fca2101ddfce80b97abc213a37a
Janet <lrosa008@gmail.com>
2016-09-14 02:08:57 -0400

    idk maybe

--- lib/potions.ex
+++ lib/potions.ex
@@ -1,6 +1,9 @@
 defmodule Potions do
   use ExCLI.DSL, escript: true
 
+  import Potions.API
+
+
   name "potions"
   description "find potions"
   long_description ~s"""
@@ -21,17 +24,7 @@ defmodule Potions do
         IO.puts("fetching ingredients.....")
       end
 
-      HTTPoison.start
-      case HTTPoison.get("http://cocktails-api.herokuapp.com/ingredients") do
-        {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-          body
-          |> Poison.decode!
-          |> Enum.each(fn %{"name" => name} -> IO.puts name end)
-        {:ok, %HTTPoison.Response{status_code: 404}} ->
-          IO.puts "Not found :("
-        {:error, %HTTPoison.Error{reason: reason}} ->
-          IO.inspect reason
-      end
+      get "ingredients"
     end
   end
 
@@ -49,31 +42,7 @@ defmodule Potions do
         IO.puts("searching for potions.....")
       end
 
-      HTTPoison.start
-      case HTTPoison.get("http://cocktails-api.herokuapp.com/ingredients") do
-        {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-          IO.puts "we printing ingredients bitch......................../\n"
-
-          body
-          |> Poison.decode!
-          |> Enum.each(fn ing -> IO.inspect ing end)
-        {:ok, %HTTPoison.Response{status_code: 404}} ->
-          IO.puts "ingredients url not found :("
-        {:error, %HTTPoison.Error{reason: reason}} ->
-          IO.inspect reason
-      end
-      case  HTTPoison.get("http://cocktails-api.herokuapp.com/cocktails") do
-        {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-          IO.puts "we printing cocktails bitch........................./\n"
-
-          body
-          |> Poison.decode!
-          |> Enum.each(fn %{"ingredient_ids" => ing_ids} -> IO.inspect ing_ids end)
-        {:ok, %HTTPoison.Response{status_code: 404}} ->
-          IO.puts "potions url not found :("
-        {:error, %HTTPoison.Error{reason: reason}} ->
-          IO.inspect reason
-      end
+      get "cocktails"
     end
   end
 end
